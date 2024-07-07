4a03196286a83a8196c37926f12dde5a6c9afc1c
CraigMLdsouza <91xfdc@gmail.com>
2024-07-07 13:50:13 +0530

    removed secret

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTPiiTpV3Fd6Oktr1k4Pfhj9T-jU11Y6k",
-  authDomain: "wairehouse-4a655.firebaseapp.com",
-  projectId: "wairehouse-4a655",
-  storageBucket: "wairehouse-4a655.appspot.com",
-  messagingSenderId: "35913707408",
-  appId: "1:35913707408:web:5463473d2a661e2841cd15",
-  measurementId: "G-L0ER909NNT"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
